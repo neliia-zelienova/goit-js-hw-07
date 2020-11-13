1a6b602e7dc02d2cb591a3bb0fd92578bc884a88
neliia-zelienova <66479463+neliia-zelienova@users.noreply.github.com>
2020-11-13 12:05:35 +0200

    Добавляет решение таск 8

---
+++ js/task-08.js
@@ -0,0 +1,51 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomColor() {
+     const colorArr = [];
+    for (let i = 0; i < 3; ++i) {
+        colorArr.push(getRandomInt(255));
+    }
+    return `rgb(${colorArr})`;
+}
+
+const createBoxes = (amount) => {
+    const boxesArray = [];
+    for (let i = 0; i < amount; ++i){
+        boxesArray.push(document.createElement('div'));
+        boxesArray[i].classList.add('created-box');
+        boxesArray[i].style.backgroundColor = getRandomColor();
+        boxesArray[i].style.width = `${30 + (i * 10)}px`;
+        boxesArray[i].style.height = `${30 + (i * 10)}px`;
+    }
+    const insidedivRef = document.querySelector('#boxes');
+    insidedivRef.append(...boxesArray);
+}
+
+const destroyBoxes = () => {
+    const insidedivRef = document.querySelector('#boxes');
+    const boxesDivArr = insidedivRef.querySelectorAll('div');
+    boxesDivArr.forEach((elem) => {
+        elem.remove();
+    });
+}
+
+const btnAction = (event) => {
+    if (event.target.dataset.action === 'render') {
+        const boxesAmountRef = document.querySelector('#controls input');
+    createBoxes(Number(boxesAmountRef.value));
+    } else {
+        destroyBoxes();
+    }
+    
+}
+ 
+const createBtnRef = document.querySelector('.task-08 button[data-action="render"]');
+const clearBtnRef = document.querySelector('.task-08 button[data-action="destroy"]');
+
+
+createBtnRef.addEventListener('click', btnAction);
+clearBtnRef.addEventListener('click', btnAction);
+
+
